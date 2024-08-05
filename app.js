334a038e373cf66e4e3f53cc3aea75b3d3a3acc4
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-08-05 15:52:23 -0400

    Seperate routes to their own files and add new message page

--- app.js
+++ app.js
@@ -2,27 +2,16 @@ const express = require('express')
 const app = express()
 const path = require('node:path')
 
+const indexRouter = require('./routes/index')
+const newRouter = require('./routes/new')
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
 
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date()
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date()
-  }
-]
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Mini Messageboard', messages: messages })
-})
+app.use('/', indexRouter)
+app.use('/new', newRouter)
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`listening on port ${PORT}!`))
